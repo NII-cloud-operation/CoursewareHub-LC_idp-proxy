a6a48b907be293a3ad3faae6639209a92bc55134
Kokichi Sasaki <k-sasaki@axsh.net>
2017-06-06 22:08:43 +0900

    added the download feature of the cloudgateway's metadata.

--- resources/simplesamlphp/bin/update_ds_metadata.sh
+++ resources/simplesamlphp/bin/update_ds_metadata.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-metadata_file=gakunin-test-metadata.xml
+ds_metadata_file="gakunin-metadata.xml"
+cg_metadata_file="attributeauthority-remote.xml"
 cd /var/www/simplesamlphp/
-/usr/bin/curl -o metadata/xml/${metadata_file} https://metadata.gakunin.nii.ac.jp/${metadata_file}
-sed -i "s|#DS_METADATA_XML#|array('type' => 'xml', 'file' => 'metadata/xml/${metadata_file}')|" config
-/config.php
+/usr/bin/curl -o metadata/xml/${ds_metadata_file} https://metadata.gakunin.nii.ac.jp/${ds_metadata_file}
+sed -i "s|#DS_METADATA_XML#|array('type' => 'xml', 'file' => 'metadata/xml/${ds_metadata_file}')|" config/config.php
+sed -i "s|#CG_METADATA_XML#|array('type' => 'xml', 'file' => 'metadata/xml/${cg_metadata_file}')|" config/config.php
+[ -e "metadata/xml/attributeauthority-remote.xml" ] || {
+/usr/bin/curl -o metadata/xml/${cg_metadata_file} https://meatwiki.nii.ac.jp/confluence/download/attachments/6684843/cgidp-metadata.xml?version=2&modificationDate=1488866091000&api=v2
+}
