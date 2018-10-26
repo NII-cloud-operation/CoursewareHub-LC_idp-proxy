47eb047050b097ad301adf2ee72d2971f1d37d97
yacchin1205 <yazawa@yzwlab.net>
2018-10-26 14:10:09 +0900

    Remove unnecessary process

--- resources/simplesamlphp/bin/add_auth_proxy.sh
+++ resources/simplesamlphp/bin/add_auth_proxy.sh
@@ -12,10 +12,5 @@ entity_id="https://$auth_proxy_host/simplesaml/module.php"
 metadata_url="https://$auth_proxy_host/simplesaml/module.php/saml/sp/metadata.php/default-sp"
 tempfile=`mktemp /tmp/xml_XXXXXX`
 curl --insecure --fail -o $tempfile $metadata_url
-chmod 666 $tempfile
-sudo docker cp $tempfile $IDP_PROXY:/root
-copyto='/root/'$(basename $tempfile)
-sudo docker exec -i $IDP_PROXY bash << EOF
-php /var/www/simplesamlphp/bin/add_auth_proxy_metadata.php $entity_id $copyto
-EOF
+php /var/www/simplesamlphp/bin/add_auth_proxy_metadata.php $entity_id $tempfile
 rm -f $tempfile
